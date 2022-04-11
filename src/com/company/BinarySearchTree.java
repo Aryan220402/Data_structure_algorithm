6019e4818b1ebdc8ea9f2498e250c8f42b81b85f
Aryan220402 <aryan.singh2_cs20@gla.ac.in>
2022-04-12 02:48:55 +0530

    BST Operation

--- src/com/company/BinarySearchTree.java
+++ src/com/company/BinarySearchTree.java
@@ -8,6 +8,7 @@ class  Node1{
         left = right = null;
     }
 }
+
 public class BinarySearchTree
 {
    static Node1 root;
@@ -52,6 +53,52 @@ public class BinarySearchTree
             return Binarysearch(root.left,key);
         }
     }
+
+
+     void delete(int key)
+    {
+        root = deleteRec(root, key);
+    }
+    Node1 deleteRec(Node1 root,int x)
+    {
+        if(root==null)
+
+            return root;
+
+        if(x<root.key)
+        {
+            root.left=deleteRec(root.left,x);
+        }
+        else if(x>root.key)
+        {
+            root.right=deleteRec(root.right,x);
+        }
+        else
+        {
+            if(root.left==null)
+            {
+                return root.right;
+            }
+            else if(root.right==null)
+            {
+                return root.left;
+            }
+            root.key=min(root.right);
+            root.right=deleteRec(root.right,root.key);
+
+        }
+        return root;
+    }
+    int min(Node1 root)
+    {
+        int minv = root.key;
+        while (root.left != null)
+        {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
     public void inOrder(Node1 root){
         if(root==null){
             return;
@@ -77,7 +124,10 @@ public class BinarySearchTree
         insert(13);
         insert(17);
         obj.inOrder(root);
-        System.out.println( obj.Binarysearch(root, 4));
+        System.out.println( obj.Binarysearch(root, 6));
+        obj.delete(6);
+        obj.inOrder(root);
+        System.out.println( obj.Binarysearch(root, 6));
 
     }
 }
